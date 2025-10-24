e6f16d33d37eaca84579fcd8491f274dc88c5764
PhatMenghor <phatmenghor19@gmail.com>
2025-10-24 14:12:29 +0700

    add

--- src/main/java/com/internal/feature/open_account/dto/request/CustomerRequest.java
+++ src/main/java/com/internal/feature/open_account/dto/request/CustomerRequest.java
@@ -12,20 +12,16 @@ public class CustomerRequest {
     @NotBlank(message = "Record ID is required")
     private String recId;
 
-    @JsonProperty("LegalId")
     @NotBlank(message = "Legal ID is required")
     private String legalId;
 
-    @JsonProperty("FamilyName")
     @NotBlank(message = "Family name is required")
     private String familyName;
     @NotBlank(message = "Given name is required")
     private String givenName;
 
-    @JsonProperty("firstNameKh")
     @NotBlank(message = "First name in Khmer is required")
     private String firstNameKh;
-    @JsonProperty("lastNameKh")
     @NotBlank(message = "Last name in Khmer is required")
     private String lastNameKh;
 
@@ -34,7 +30,6 @@ public class CustomerRequest {
 
     private String legalAddress;
     
-    @JsonProperty("Gender")
     @NotBlank(message = "Gender is required")
     private String gender;
     
@@ -48,14 +43,10 @@ public class CustomerRequest {
 
     private String releasedBy;
     
-    @JsonProperty("AverageIncome")
     private String averageIncome;
 
-    @JsonProperty("LegalDocName")
-    @NotBlank(message = "Legal document name is required")
     private String legalDocName;
     
-    @JsonProperty("Occupation")
     private String occupation;
     
     private String customerProvince;
@@ -66,11 +57,9 @@ public class CustomerRequest {
     @NotBlank(message = "Phone number is required")
     private String phoneNumber;
     
-    @JsonProperty("NID_IMAGE")
     @NotBlank(message = "NID image is required")
     private String nidImage;
     
-    @JsonProperty("SELFIE_IMAGE")
     @NotBlank(message = "Selfie image is required")
     private String selfieImage;
 }
