3bfca2b56bab5700dc8482f8f646d1b788dc2318
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-01-22 15:54:55 +0700

    Auto commit on 2026-01-22 15:54:55

--- a_push.sh
+++ a_push.sh
@@ -10,6 +10,6 @@ git add .
 git commit -m "Auto commit on $CURRENT_TIME"
 
 # Push to development branch
-git push origin main
+git push origin master
 
 echo "✅ Code pushed to 'development' branch at $CURRENT_TIME"
