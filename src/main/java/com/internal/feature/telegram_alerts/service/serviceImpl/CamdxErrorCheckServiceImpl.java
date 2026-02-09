d1c325d41f7adfe9abac48732a4b3d856ed7c680
Makara <makarakara79@gmail.com>
2026-02-09 16:46:16 +0700

    change incorrect field to english instead of khmer to match with label

--- src/main/java/com/internal/feature/telegram_alerts/service/serviceImpl/CamdxErrorCheckServiceImpl.java
+++ src/main/java/com/internal/feature/telegram_alerts/service/serviceImpl/CamdxErrorCheckServiceImpl.java
@@ -190,26 +190,13 @@ public class CamdxErrorCheckServiceImpl implements ErrorAlertsCamdxService {
         if (incorrectFields != null && !incorrectFields.isEmpty()) {
             StringBuilder sbIncorrect = new StringBuilder();
             for (String field : incorrectFields) {
-                String translatedField = checkField(field);
-                sbIncorrect.append(AppConstants.BULLET_PREFIX).append(translatedField).append(AppConstants.NEW_LINE);
+                sbIncorrect.append(AppConstants.BULLET_PREFIX)
+                        .append(field)
+                        .append(AppConstants.NEW_LINE);
             }
             return sbIncorrect.toString().trim();
         } else {
             return AppConstants.FIELD_NONE;
         }
-    }
-
-    private static String checkField(String field) {
-        String translatedField = field;
-        if (AppConstants.FIELD_LAST_NAME_EN.equalsIgnoreCase(field)) {
-            translatedField = AppConstants.FIELD_KH_LASTNAME_EN;
-        } else if (AppConstants.FIELD_FIRST_NAME_EN.equalsIgnoreCase(field)) {
-            translatedField = AppConstants.FIELD_KH_FIRSTNAME_EN;
-        } else if (AppConstants.FIELD_DOB.equalsIgnoreCase(field)) {
-            translatedField = AppConstants.FIELD_KH_DOB;
-        } else if (AppConstants.FIELD_GENDER.equalsIgnoreCase(field)) {
-            translatedField = AppConstants.FIELD_KH_GENDER;
-        }
-        return translatedField;
     }
 }
